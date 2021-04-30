30cadf282f9a53818238f2648736b15687381a59
Icarus <ibuckhold@gmail.com>
2021-04-29 21:04:24 -0600

    fixed small styling issues

--- app/api/feed_routes.py
+++ app/api/feed_routes.py
@@ -9,17 +9,18 @@ feed_routes = Blueprint('feed', __name__)
 @feed_routes.route('')
 @login_required
 def user_feed():
-  following = {
+    following = {
         follow.username: follow.username for follow in current_user.follows}
-  followers = {
+    followers = {
         followers.username: followers.username for followers in current_user.followers}
-  followerUsername = [key for key in following]
+    followerUsername = [key for key in following]
 
-  followingPosts = Post.query.join(User).filter(User.username.in_(followerUsername)).all()
-  print('--------------------->', followerUsername)
+    followingPosts = Post.query.join(User).filter(
+        User.username.in_(followerUsername)).all()
+    print('--------------------->', followerUsername)
 
-  return {"userDict": current_user.to_user_dict(),
-              "following": following,
-              "followers": followers,
-              'followingPosts': [post.to_dict() for post in followingPosts]
-              }
+    return {"userDict": current_user.to_user_dict(),
+            "following": following,
+            "followers": followers,
+            'followingPosts': [post.to_dict() for post in followingPosts]
+            }
