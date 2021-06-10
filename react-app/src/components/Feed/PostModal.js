79e1e5fa5fcaa79aff85ffd9228a1f3b6b8a7ed4
DZhou005 <74795454+DZhou005@users.noreply.github.com>
2021-06-09 18:34:03 -0700

    comments done, need to do likes

--- react-app/src/components/Feed/PostModal.js
+++ react-app/src/components/Feed/PostModal.js
@@ -1,34 +1,37 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { useParams } from 'react-router-dom';
+import { useParams, Link } from 'react-router-dom';
 import { getOnePost } from '../../store/feed';
+import './Feed.css'
 
 function PostModal({ postId }) {
-  // const post = useSelector(state => state.post)
-  console.log(postId, '-----');
   const dispatch = useDispatch();
-  // let id = useParams();
-  const post = useSelector(state => state.feedReducer?.selectedPost);
+  const post = useSelector(state => state.feedReducer?.selectedPost?.currentPost);
   console.log(post);
-
-  // useEffect(() => {
-  //   (async () => {
-  //     await dispatch(getOnePost(postId));
-  //   })()
-  // }, [dispatch])
 
   if (!post) return null;
 
   return (
-    <>
-      <h1>TEST</h1>
-      <div>{post?.user.username}</div>
-      {/* <img src={post?.imageUrl}></img> */}
-      <div>{post?.caption}</div>
-      {post?.comments.map((comment) => (
-        <div key={comment.id}>{comment.body}</div>
-      ))}
-    </>
+    <div className='modalContainer'>
+      <img className='postModalImages'src={post?.imageUrl}></img>
+      <div className='modalUserName'>
+        <Link to={`/${post?.user?.username}`} className='far fa-user-circle  modalUserNameText'>&nbsp;&nbsp;&nbsp;&nbsp;{post?.user.username}</Link>
+        <div className='modalCaption'>{post?.caption}</div>
+        <div className='line'></div>
+        <div className='modalCommentsContainer'>
+          {post?.comments.map((comment) => (
+            <div>
+              <Link className='far fa-user-circle modalCommentUser' to={`/${comment.userId.username}`}>&nbsp;&nbsp;&nbsp;&nbsp;{comment.userId.username}</Link>
+              <div key={comment.id} className='modalComments'>{comment.body}</div>
+            </div>
+        ))}
+        </div>
+        <div className='modalCommentBar'>
+          <input placeholder='add a comment...' className='modalCommentInput'></input>
+        </div>
+      </div>
+
+    </div>
   )
 }
 
